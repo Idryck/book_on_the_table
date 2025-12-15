2ce1d86971a15fe37a9338e775a5e89abbf4a8bf
Lucas Souza <139274653+Idryck@users.noreply.github.com>
2025-12-15 11:55:31 +0000

    coloca o prettierignore

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,9 @@
-function home (){
+function home() {
   return (
-
     <div>
       <h1>Bem vindos ao Book on the Table! </h1>
       <p>com as hitórias mais mirabolantes.</p>
     </div>
-  )
+  );
 }
 export default home;
