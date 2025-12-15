868a84819a0e0ba7e6d80d21a5b64b97a7af4d6e
Lucas Souza <139274653+Idryck@users.noreply.github.com>
2025-12-15 11:39:00 +0000

    commit da fundação, com o `prettier`, `editorconfig`, `gitignore` e as `dependencias`

---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+function home (){
+  return (
+
+    <div>
+      <h1>Bem vindos ao Book on the Table! </h1>
+      <p>com as hitórias mais mirabolantes.</p>
+    </div>
+  )
+}
+export default home;
